2a37889bf44490c8a386e0a4337702cd53572429
limss <fruits2lim@gmail.com>
2025-12-09 20:34:00 +0900

    모니터링툴 Sentry 연동 & Dockerfile 작성 & docker-compose.yml 작성

--- frontend/config/openapi-runtime.ts
+++ frontend/config/openapi-runtime.ts
@@ -4,8 +4,10 @@ import { getCookie } from "cookies-next/server";
 import { cookies } from "next/headers";
 
 const AUTH_COOKIE_NAME =
-  process.env.NODE_ENV === "production"
-    ? "__Secure-authjs.session-token"
+  // process.env.NODE_ENV === "production"
+  // production 이더라도 HTTP로 동작할 수도 있음 / 로컬호스트 환경에서 production으로 테스트해볼 수 있음
+  process.env.USE_HTTPS === "true"
+    ? "__Secure-authjs.session-token" // Secure Cookie는 원칙적으로 HTTPS 환경에서만 실행되어야함 
     : "authjs.session-token";
 
 const API_URL = process.env.API_URL || "http://localhost:8000";
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,3 +1,4 @@
+import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -12,18 +13,53 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.CLOUDFRONT_DOMAIN as string,
-      },
+      ...(process.env.CLOUDFRONT_DOMAIN // CLOUDFRONT_DOMAIN 이 있으면 기존 설정을 넣어주고, 없을 때는 아무것도 안 넣어줌 -> null 방지
+        ? [
+            {
+              protocol: "https" as const,
+              hostname: process.env.CLOUDFRONT_DOMAIN as string,
+            },
+          ]
+        : []),
 
       // Course seed에 있는 host값
       {
-        protocol: "https",
+        protocol: "https" as const,
         hostname: "cdn.inflearn.com",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+  org: "seongsil",
+
+  project: "inflearn-clone-web",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+});
