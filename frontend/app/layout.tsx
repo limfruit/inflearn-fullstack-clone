1859ac08123d7975f66bd7227db265e4a80b6a0e
limss <fruits2lim@gmail.com>
2025-12-12 13:25:36 +0900

    Dockerfile 수정 & 테스트 배포

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -39,6 +39,7 @@ export default async function RootLayout({
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-7xl mx-auto`}
       >
+        테스트중...
         <Providers>
           <SiteHeader
             session={session}
